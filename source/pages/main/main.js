796b07d1cd90305d0fccf582dd213bc0c926dbe0
Yuliya Saltrukovich <yulya_9060@mail.ru>
2018-01-03 18:58:59 +0300

    реализовано текстовое поле с очисткой

--- source/pages/main/main.js
+++ source/pages/main/main.js
@@ -1,3 +1,22 @@
 import './main.scss';
 import '../../fonts/fonts.scss';
-import '../../img/favicon.jpg'
+import '../../images/favicon.jpg';
+
+var span = document.querySelector('.input-box__span');
+var input = document.getElementById('menu-input');
+input.addEventListener('input',addModify);
+
+function addModify() {
+    var inputLength = event.target.value.length;
+    span.classList.add('input-box__span_visibility');
+if (inputLength == 0){
+    span.classList.remove('input-box__span_visibility');
+}
+  }; 
+
+span.addEventListener('click',clearInput);
+
+function clearInput(){
+    input.value='';
+}
+  
